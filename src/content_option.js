75040c5be144a648a8b6293429f782ef21ae6798
ridhik000ks <ridhik000ks@gmail.com>
2024-08-06 14:16:53 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,125 @@
+const logotext = "RIDHIK";
+const meta = {
+    title: "RIDHIK K S",
+    description: "I’m RIDHIK K S, A Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m RIDHIK K S",
+    animated: {
+        first: "Passionate about software development",
+        second: "Build engaging and interactive web experiences.",
+        third: "Develop functional and intuitive mobile apps.",
+    },
+    description: "Crafting exceptional web and mobile applications. Proficient in React Native and e-learning solutions. Let's collaborate on your next innovative project.",
+    your_img_url: require("./assets/images/dp2.jpg"),
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I’m Ridhik, a full-stack developer passionate about crafting digital solutions. With expertise in web and mobile development, I specialize in creating user-friendly apps and websites. Let’s build something amazing together.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [
+    {
+        name: "HTML",
+        value: 100,
+    },
+    {
+        name: "CSS",
+        value: 100,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "React Native",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "Python",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 50,
+    },
+    {
+        name: "BootStrap",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "Web Application",
+        description: "I specialize in building robust and scalable web applications that deliver exceptional user experiences. From concept to deployment, I handle all aspects of development, including front-end design, back-end logic, and database integration.",
+    },
+    {
+        title: "Mobile Application",
+        description: "I create engaging and user-centric mobile applications for both iOS and Android platforms. With a focus on performance and usability, I deliver apps that meet the needs of your target audience.",
+    },
+];
+
+const dataportfolio = [{
+        img: require("./assets/images/e-learning.jpg"),
+        description: "E-learning app with user-friendly interface, course tracking, and video lessons.",
+        link: "https://github.com/ridhik-ks/e_learning.git",
+    },
+    {
+        img: require("./assets/images/weatherApp.jpg"),
+        description: "Get real-time Kerala weather updates with our app. Includes current conditions, forecasts, and alerts for major cities.",
+        link: "https://github.com/ridhik-ks/weather_app.git",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ridhik000ks@gmail.com.com",
+    YOUR_FONE: "+91 9746186485",
+    description: "Have a project in mind or need assistance? Feel free to reach out. I'm eager to collaborate on your next digital endeavor.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/ridhik-ks/ridhik-ks.git",
+    facebook: "https://www.facebook.com/ridhik.ks/",
+    linkedin: "https://www.linkedin.com/in/ridhik-ks/",
+    twitter: "https://x.com/ridhik_ks",
+    instagram: "https://www.instagram.com/__.ridhik_ks.__/"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
